39eda1f0f24b4fda3ae18cab1ba794a84f7e8ca2
Frederic Folon <frederic.folon@social.aston-ecole.com>
2021-03-19 12:07:13 +0100

    Connexion Session Logout

--- Dev/main.py
+++ Dev/main.py
@@ -49,3 +49,13 @@ def login():
             msg = 'Incorrect username/password!'
 
     return render_template('index.html', msg='')
+
+# http://localhost:5000/python/logout - this will be the logout page
+@app.route('/pythonlogin/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))

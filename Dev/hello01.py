9dd7f8ed24a43b227ad332862ae59489370495ff
Frederic Folon <frederic.folon@social.aston-ecole.com>
2021-03-19 16:40:17 +0100

    Ajout layout

--- Dev/hello01.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from markupsafe import escape
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return f"<p>Hello, {escape(name)}!</p>"
--- Dev/main.py
+++ Dev/main.py
@@ -1,25 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 # $ pip install flask-mysqldb
-from flask_mysqldb import MySQLgit
+from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
+# import re
 
 app = Flask(__name__)
 
-# Change this to your secret key (can be anything, it's for extra protection)
-app.secret_key = 'your secret key'
-
-# Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
+# Entrez les détails de votre connexion à la base de données ci-dessous
+app.config['MYSQL_HOST'] = '51.254.227.51'
 app.config['MYSQL_USER'] = 'pma'
 app.config['MYSQL_PASSWORD'] = 'aka47-Pma'
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_DB'] = 'areyouhere'
 
 # Intialize MySQL
 mysql = MySQL(app)
 
-# http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
-@app.route('/pythonlogin/', methods=['GET', 'POST'])
+# http://localhost:5000/ - this will be the login page, we need to use both GET and POST requests
+@app.route('/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
     msg = ''
@@ -32,6 +29,7 @@ def login():
 
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        mysql.connection.cursor(MySQLdb.c)
         cursor.execute('SELECT * FROM FORMATEUR WHERE email = %s AND password = %s', (username, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
@@ -41,21 +39,45 @@ def login():
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
-            session['username'] = account['username']
+            session['email'] = account['email']
             # Redirect to home page
             return 'Logged in successfully!'
         else:
             # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
+            msg = 'Incorrect email/password!'
 
     return render_template('index.html', msg='')
 
-# http://localhost:5000/python/logout - this will be the logout page
-@app.route('/pythonlogin/logout')
+# http://localhost:5000/logout - this will be the logout page
+@app.route('/logout')
 def logout():
     # Remove session data, this will log the user out
    session.pop('loggedin', None)
    session.pop('id', None)
-   session.pop('username', None)
+   session.pop('email', None)
    # Redirect to login page
    return redirect(url_for('login'))
+
+# http://localhost:5000/home - this will be the home page, only accessible for loggedin users
+@app.route('/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', email=session['email'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+# http://localhost:5000/profile - this will be the profile page, only accessible for loggedin users
+@app.route('/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM FORMATEUR WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))

44e87a84fb982d67f910bd8890cca65ee4b48e6b
Alexandre Deresnes <alexandre.deresnes@gmail.com>
2021-05-06 10:06:24 +0200

    debug sonar code smell security

--- app/config.py
+++ app/config.py
@@ -2,8 +2,3 @@ MYSQL_DB = "areyouhere"
 MYSQL_HOST = "db"
 MYSQL_USER = "areyouhere"
 MYSQL_PASSWORD = "aka47-areyouhere"
-
-LOGSTASH_HOST = "51.254.227.51"
-#LOGSTASH_DB_PATH = "/home/vagrant/app-data/flask_logstash.db"
-#LOGSTASH_TRANSPORT = "logstash_async.transport.BeatsTransport"
-LOGSTASH_PORT = 5001
